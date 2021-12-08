4e9f3f7ea6621860de25522d995846abd34025d5
Genics85 <eugeneamo85@gmail.com>
2021-12-08 12:05:57 +0000

    cleaned the FINALsERIAL

--- slave/slave.ino
+++ slave/slave.ino
@@ -10,79 +10,65 @@ bool stringComplete = false;
 bool detectState=false;
 
 void setup() {
-pinMode(ldr,INPUT);
-pinMode(sensor,INPUT);
-pinMode(button,INPUT);
-inputString.reserve(200);
-pinMode(button,INPUT_PULLUP);
-attachInterrupt(digitalPinToInterrupt(3),InterruptFunction,RISING);
-Serial.begin(38400);
- 
+  pinMode(ldr,INPUT);
+  pinMode(sensor,INPUT);
+  pinMode(button,INPUT);
+  inputString.reserve(200);
+  pinMode(button,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(3),InterruptFunction,RISING);
+  Serial.begin(38400);
 }
 
 void loop() {
   
-bool detected=digitalRead(sensor);
-bool buttonRead=digitalRead(button);
-ldrvalue=analogRead(ldr);
+  bool detected=digitalRead(sensor);
+  bool buttonRead=digitalRead(button);
+  ldrvalue=analogRead(ldr);
 
-
-if(detected==HIGH && detectState==false){
-  Serial.println("motion");
-  detectState=true;
+  //Prints out the state of the motion detector
+  if(detected==HIGH && detectState==false){
+    Serial.println("motion");
+    detectState=true;
   }
   else if(detected==LOW && detectState==true){
-    Serial.println("no motion");
-    detectState=false;
+      Serial.println("no motion");
+      detectState=false;
   }
 
+//runs when the data being received from the slave is complete
+  if(stringComplete && inter==true){
+    //cleans the received data
+    inputString.trim();
+    Serial.println(inputString);
 
-if(stringComplete && inter==true){
- 
-  inputString.trim();
-  Serial.println(inputString);
-
-  if(inputString=="negative"){
-    state="OFF";
+  //Changes the state of the system when the slave toggles the light
+    if(inputString=="negative"){
+      state="OFF";
+    }
+    else if(inputString=="positive"){
+      state="ON";
+    }
+      inputString = "";
+      stringComplete = false;
+      //sets the interrupts state to false after we are done with the interrupt
+      inter=false;
+      delay(15000);
   }
-  else if(inputString=="positive"){
-    state="ON";
-  }
-  
-  inputString = "";
-    stringComplete = false;
-    inter=false;
-
-
-    delay(15000);
-   
-
+  //Switches on the light with the condition of the state, ldr value and the interrupt state
+  if( state== "OFF" && ldrvalue > 50 && inter==false){
+      Serial.println("ON");
+      state=" ON";
+  }  
+    //Switches on the light with the condition of the state, ldr value, motion sensor and the interrupt state
+  else if (state=="OFF" && (ldrvalue < 50) && detected == HIGH && inter==false){
+      Serial.println("ON");
+      state="ON";
+  }   
 }
-
-
-
- if( state== "OFF" && ldrvalue > 50 && inter==false){
-  
-    Serial.println("ON");
-    state=" ON";
-  
-    
-  }
-  
-else if (state=="OFF" && (ldrvalue < 50) && detected == HIGH && inter==false){
-  
-    Serial.println("ON");
-    state="ON";
-  
-}
-    
-  }
-
-    void serialEvent() {
+//Receiving data from the slave
+void serialEvent() {
   while (Serial.available()) {
-
     char inChar = (char)Serial.read();
-
     inputString += inChar;
     
     if (inChar == '\n') {
@@ -90,13 +76,12 @@ else if (state=="OFF" && (ldrvalue < 50) && detected == HIGH && inter==false){
     }
   }
 }
-
-  void InterruptFunction(){
-    
+//sends INTERRUPT to the slave
+void InterruptFunction(){
     Serial.println("INTERRUPT");
+    //sets the interrupt state to true
     inter=true;
-    
-  }
+}
   
 
 
